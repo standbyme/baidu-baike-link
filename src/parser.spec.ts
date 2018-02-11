e10497c3e2a9c2e2709de9a74614fb2cd1cacc73
TGS <tgs@tgs.blue>
2018-02-11 15:18:23 +0800

    add get_links

--- src/parser.spec.ts
+++ src/parser.spec.ts
@@ -1,6 +1,6 @@
 import * as assert from 'assert'
 
-import { is_one } from './parser'
+import { get_links, is_one } from './parser'
 import { get_html } from './utility'
 
 describe('One or Many', function () {
@@ -11,5 +11,14 @@ describe('One or Many', function () {
     it('basic test', async function () {
         const html = (await get_html('https://baike.baidu.com/item/%E9%99%86%E5%8A%B1%E6%88%90?force=1')).get()
         assert(is_one('陆励成', html))
+    })
+})
+
+describe('Get Links', function () {
+    it('basic test', async function () {
+        const html = (await get_html('https://baike.baidu.com/item/%E6%9D%8E%E5%85%8B%E5%BC%BA?force=1')).get()
+        const result = get_links(html)
+        assert.equal(result.length, 7)
+        assert.equal(result[6], 'https://baike.baidu.com/item/%E6%9D%8E%E5%85%8B%E5%BC%BA/20595314')
     })
 })
--- src/parser.ts
+++ src/parser.ts
@@ -1,7 +1,18 @@
+import { Either, Option } from 'funfix-core'
+
+import { get_html, URL_constructor } from './utility'
+
 function isContains(str: string, substr: string): boolean {
     return str.indexOf(substr) >= 0
 }
 
 export function is_one(name: string, original_html: CheerioStatic): boolean {
     return !isContains(original_html('title').text(), `${name}（${name}）`)
+}
+
+export function get_links(original_html: CheerioStatic): string[] {
+    const result = original_html('.para-list').find('a').map(function () {
+        return original_html(this).attr('href')
+    }).get().map(l => `https://baike.baidu.com${l}`)
+    return result
 }
