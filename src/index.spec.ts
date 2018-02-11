effb5981d6bca543d2cc2331d1259ff8a6941089
TGS <tgs@tgs.blue>
2018-02-11 17:44:29 +0800

    change sign

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -2,28 +2,20 @@ import * as assert from 'assert'
 
 import index from './index'
 
-describe('One or Many', function () {
-    it('should return None when it does not exist', async function () {
+describe('Index', function () {
+    it('should return [] when it does not exist', async function () {
         const result = await index('表格存储')
-        assert(result.isEmpty())
+        assert(result.length === 0)
     })
-    it('should return Some Left when it exists and has many links', async function () {
-        const result__opt = await index('韩庚')
-        assert(result__opt.nonEmpty())
-        const result = result__opt.get()
-        assert(result.isLeft())
-        // @ts-ignore
-        const left: string[] = result.swap().get()
-        assert.equal(left.length, 2)
-        assert.equal(left[0], 'https://baike.baidu.com/item/%E9%9F%A9%E5%BA%9A/5909')
+    it('has many links', async function () {
+        const result = await index('韩庚')
+        assert.equal(result.length, 2)
+        assert.equal(result[0], 'https://baike.baidu.com/item/%E9%9F%A9%E5%BA%9A/5909')
     })
     it('should return Some Right when it exists and has one link', async function () {
         //
-        const result__opt = await index('百度百科')
-        assert(result__opt.nonEmpty())
-        const result = result__opt.get()
-        assert(result.isRight())
-        const right = result.get()
-        assert.equal(right, 'https://baike.baidu.com/item/%E7%99%BE%E5%BA%A6%E7%99%BE%E7%A7%91?force=1')
+        const result = await index('百度百科')
+        assert(result.length === 1)
+        assert.equal(result[0], 'https://baike.baidu.com/item/%E7%99%BE%E5%BA%A6%E7%99%BE%E7%A7%91?force=1')
     })
 })
--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,13 @@ import { Either, Option } from 'funfix-core'
 import { get_links, is_one } from './parser'
 import { get_html, URL_constructor } from './utility'
 
-function helper(name: string, url: string, html: CheerioStatic): Either<string[], string> {
-    if (is_one(name, html)) return Either.right(url)
-    else return Either.left(get_links(html))
+function helper(name: string, url: string, html: CheerioStatic): string[] {
+    if (is_one(name, html)) return [url]
+    else return get_links(html)
 }
 
-export default async function (name: string): Promise<Option<Either<string[], string>>> {
+export default async function (name: string): Promise<string[]> {
     const url = URL_constructor(name)
     const html__opt = await get_html(url)
-    return html__opt.map(html => helper(name, url, html))
+    return html__opt.map(html => helper(name, url, html)).getOrElse([])
 }
