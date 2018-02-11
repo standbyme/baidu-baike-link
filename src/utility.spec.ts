b01c8011ec35e8c729f53ad595fe50a4537b4ad8
TGS <tgs@tgs.blue>
2018-02-11 14:38:29 +0800

    get_html

--- src/utility.spec.ts
+++ src/utility.spec.ts
@@ -1,10 +1,26 @@
 import * as assert from 'assert'
-import { URL_constructor } from './utility'
+
+import { get_html, URL_constructor } from './utility'
 
 describe('URL_constructor', function () {
     it('basic test', async function () {
         const mock = '李克强'
         const result = URL_constructor(mock)
         assert.equal('https://baike.baidu.com/item/%E6%9D%8E%E5%85%8B%E5%BC%BA?force=1', result)
+    })
+})
+
+describe('Get HTML', function () {
+    this.slow(5000)
+    this.timeout(10000)
+    it('should return None if url is not of baike content', async function () {
+        const mock_url = 'https://baike.baidu.com/item/%e6%9b%b2%e7%9a%84%e6%ad%8c?force=1'
+        const html = await get_html(mock_url)
+        assert(html.isEmpty())
+    })
+    it('should return Some if url is of baike content', async function () {
+        const mock_url = 'https://baike.baidu.com/item/%E6%9D%8E%E5%85%8B%E5%BC%BA?force=1'
+        const html = await get_html(mock_url)
+        assert(html.nonEmpty())
     })
 })
--- src/utility.ts
+++ src/utility.ts
@@ -1,4 +1,19 @@
+import axios from 'axios'
+import * as Cheerio from 'cheerio'
+import { Option } from 'funfix-core'
+
 export function URL_constructor(name: string): string {
     const result = `https://baike.baidu.com/item/${encodeURIComponent(name)}?force=1`
     return result
+}
+
+export async function get_html(url: string): Promise<Option<CheerioStatic>> {
+    const { data, request } = await axios.get(url)
+    const item_url = request.res.responseUrl
+    if (item_url !== 'https://baike.baidu.com/error.html') {
+        const $ = Cheerio.load(data)
+        return Option.of($)
+    } else {
+        return Option.none()
+    }
 }
